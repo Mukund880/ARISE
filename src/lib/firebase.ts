8445551bf6b8bfd9bd1de4785022252f5ccb3db3
Mukund880 <mukundganeshkumarg@hotmail.com>
2026-05-27 18:38:48 +0530

    refactor: externalize Firebase credentials to client-side environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,14 +4,14 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBAklY1ikdBguVgwcVvQtzJeiov8Na4V2k",
-  authDomain: "arise-lms.firebaseapp.com",
-  databaseURL: "https://arise-lms-default-rtdb.firebaseio.com",
-  projectId: "arise-lms",
-  storageBucket: "arise-lms.firebasestorage.app",
-  messagingSenderId: "902839612824",
-  appId: "1:902839612824:web:9b1b682c93c8e732f427ed",
-  measurementId: "G-Z23LT4F8W7"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
